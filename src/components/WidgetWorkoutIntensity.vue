b554e1bb8d04062855c2ce7bd03591c45f2af3b1
Warren Byron <wbyron@shoprite.co.za>
2023-12-03 16:38:22 +0000

    Activities form widgets finishing for now...

---
+++ src/components/WidgetWorkoutIntensity.vue
@@ -0,0 +1,31 @@
+{
+"description": "low",
+"percentage": 10.0
+{
+"description": "light",
+            percentage = 20.0
+{
+"description": "moderate",
+            percentage = 30.0
+{
+"description": "low-to-moderate",
+            percentage = 40.0
+{
+"description": "moderate-intensity",
+            percentage = 60.0
+{
+"description": "high",
+            percentage = 70.0
+{
+"description": "high-intensity",
+            percentage = 80.0
+{
+"description": "very high",
+            percentage = 90.0
+{
+"description": "extreme",
+            percentage = 100.0
+{
+"description": "varied",
+            percentage = 50.0
+ 
